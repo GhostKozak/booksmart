6bc1844c776c7a8c769a8625581d34c87a2d64f2
GhostKozak <codeodd3@gmail.com>
2026-02-14 20:20:10 +0300

    perf: optimization of bundle size using manual chunks in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          recharts: ['recharts'],
+          dndkit: ['@dnd-kit/core', '@dnd-kit/sortable', '@dnd-kit/utilities'],
+          vendor: ['react', 'react-dom', 'dexie', 'dexie-react-hooks'],
+        }
+      }
+    }
+  }
 })
